a6ef3d3dc037ea7e945e89d65e99fddba48d0bf4
Gurkiratsingh Kohli <62511071+Gurkiratsinghk@users.noreply.github.com>
2025-08-07 09:52:46 +0530

    update: map_border.py
    changed noise with perlin-noise in requirements.txt
    updated map_border.py accordingly

--- src/map_border.py
+++ src/map_border.py
@@ -1,20 +1,14 @@
 import numpy as np
-import noise
+from perlin_noise import PerlinNoise
 import cv2
 
 def generate_border(width, height, scale=100.0, octaves=6, persistence=0.5, lacunarity=2.0):
     """Generates a natural-looking map border using Perlin noise."""
+    perlin = PerlinNoise(octaves=octaves, seed=0)
     border = np.zeros((height, width))
     for i in range(height):
         for j in range(width):
-            border[i][j] = noise.pnoise2(i/scale,
-                                         j/scale,
-                                         octaves=octaves,
-                                         persistence=persistence,
-                                         lacunarity=lacunarity,
-                                         repeatx=width,
-                                         repeaty=height,
-                                         base=0)
+            border[i][j] = perlin([i/scale, j/scale])
 
     # Normalize the border to be between 0 and 255
     border = ((border - border.min()) * (1/(border.max() - border.min()) * 255)).astype(np.uint8)
